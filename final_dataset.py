03f8a66033d7e58aa8656faed82ccff621051648
nsivaku <nsivaku@unc.edu>
2023-09-30 22:20:19 -0400

    updated dataset

--- final_dataset.py
+++ final_dataset.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from scipy import stats
 
 bangers = pd.read_csv('banger_features.csv').drop(['track.name'], axis=1)
 nonbangers = pd.read_csv('nonbanger_features.csv')
@@ -7,4 +8,11 @@ bangers['is_banger'] = 'yes'
 nonbangers['is_banger'] = 'no'
 
 final_dataset = pd.concat([bangers, nonbangers], axis=0, ignore_index=True).drop(['Unnamed: 0'], axis=1)
+
+columns = list(final_dataset.columns[1:-1])
+
+for column in columns:
+    final_dataset[column] = stats.zscore(final_dataset[column])
+
 final_dataset.to_csv('audio_features.csv', index=False)
+
